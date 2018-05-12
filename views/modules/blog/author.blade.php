8a776600aff07369c25388ca494865ae092de288
aniliso <26839479+aniliso@users.noreply.github.com>
2018-05-13 02:47:23 +0300

    Theme Starting

---
+++ views/modules/blog/author.blade.php
@@ -0,0 +1,14 @@
+@extends('blog::layouts.master')
+
+@section('blog-breadcrumbs')
+    @component('partials.components.breadcrumbs', ['breadcrumb'=>'blog.author', 'header_type'=>1])
+        {{ trans('themes::blog.author posts', ['author'=>$author->fullname]) }}
+    @endcomponent
+@endsection
+
+@section('blog-content')
+    @foreach($posts as $post)
+        @include('blog::partials._post')
+    @endforeach
+    {!! $posts->render('partials.components.pagination') !!}
+@endsection
---
+++ views/modules/blog/category.blade.php
@@ -0,0 +1,14 @@
+@extends('blog::layouts.master')
+
+@section('blog-breadcrumbs')
+    @component('partials.components.breadcrumbs', ['breadcrumb'=>'blog.category', 'header_type'=>1])
+        {{ $category->name }}
+    @endcomponent
+@endsection
+
+@section('blog-content')
+    @foreach($posts as $post)
+        @include('blog::partials._post')
+    @endforeach
+    {!! $posts->render('partials.components.pagination') !!}
+@endsection
